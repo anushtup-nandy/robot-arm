e4284cb4a77db7b89eb7a106f52156990fb99580
anushtup <110029497+anushtup-nandy@users.noreply.github.com>
2024-06-24 19:42:26 +0530

    first compilation

--- test/eigen.cpp
+++ test/eigen.cpp
@@ -4,10 +4,11 @@
 int main()
 {
 	std::cout<<"hello"<<std::endl;
-	Eigen::MatrixXd m(2, 2);
-	m(0,0) = 3;
-	  m(1,0) = 2.5;
-	  m(0,1) = -1;
-	  m(1,1) = m(1,0) + m(0,1);
-	  std::cout << m << std::endl;
+	//EXAMPLE-1
+	/* Eigen::MatrixXd m(2, 2); */
+	/* m(0,0) = 3; */
+	/*   m(1,0) = 2.5; */
+	/*   m(0,1) = -1; */
+	/*   m(1,1) = m(1,0) + m(0,1); */
+	/*   std::cout << m << std::endl; */
 }
